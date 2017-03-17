f9155f9696be174d8c3a75e508b7e432a99f1ad2
Nick Michaels <nickmichaels@gmail.com>
2017-03-17 15:59:47 -0400

    Docstrings and PEP8
    Modified all classes to include proper docstrings and pep8 compliance

--- road.py
+++ road.py
@@ -2,53 +2,67 @@ import random
 
 
 class Road:
-    """The summary line for a class docstring should fit on one line.
+    """ Class to create a "road" that will have "signals" and "vehicles"
 
     Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
-
+        name (str): Name of the road.
+        length (int): Length of the road in miles
+        speed_limit (int): Speed limit of road in MPH.
     """
-    
+
     def __init__(self, **kwargs):
         defaults = {
             'name': 'Road ' + str(random.randint(1, 99999)),
             'length': '15',
             'speed_limit': '55'
         }
-        
+
         for key, default in defaults.items():
             setattr(self, key, kwargs.get(key, default))
 
-    def tell_road_name(self):
+    def state_road_name(self):
+        """
+        Returns a formatted string stating the name of the road
+        """
         return 'The name of this road is {}'.format(self.name)
 
-    def tell_road_length(self):
+    def state_road_length(self):
+        """
+        Returns a formatted string stating the length of the road in miles.
+        """
         return 'The length of {} is {} miles'.format(self.name,
                                                      self.length)
 
-    def tell_speed_limit(self):
+    def state_speed_limit(self):
+        """
+        Returns a formatted string stating the speed limit in MPH.
+        """
         return 'The speed limit of {} is {} MPH'.format(self.name,
                                                         self.speed_limit)
 
-    @staticmethod    
+    @staticmethod
     def generate_random_road_data():
+        """
+        Generate random signal data to pass to a new road object
+
+        Returns:
+            A dict of data to pass to a new road object
+        """
         data = {
-            'name': 'Route ' + str(random.randint(10, 999)), 
-            'length': random.randint(10, 100), 
+            'name': 'Route ' + str(random.randint(10, 999)),
+            'length': random.randint(10, 100),
             'speed_limit': random.randint(25, 85)
         }
-        
-        return data
 
+        return data
 
 if __name__ == "__main__":
     data = Road.generate_random_road_data()
     x = Road(**data)
-    print(x.tell_road_name())
-    print(x.tell_road_length())
-    print(x.tell_speed_limit())
+    print(x.state_road_name())
+    print(x.state_road_length())
+    print(x.state_speed_limit())
     y = Road()
-    print(y.tell_road_name())
-    print(y.tell_road_length())
-    print(y.tell_speed_limit())
+    print(y.state_road_name())
+    print(y.state_road_length())
+    print(y.state_speed_limit())
--- signal.py
+++ signal.py
@@ -2,59 +2,86 @@ import random
 
 
 class Signal:
-    """The summary line for a class docstring should fit on one line.
+    """ Class to create a "signal" that will be placed on a road
 
     Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
-
+        location (str): Location of the signal, should correspond to a mile
+                        marker within the confines of a road.
+        r_cycle (int): Amount of time in seconds that the signal is red.
+        g_cycle (int): Amount of time in seconds that the signal is green.
+        offset (int): Amount of time in seconds that the lights start time
+                      is offset from a standard start time
     """
 
     def __init__(self, **kwargs):
         defaults = {
-            'location': '2', #  just in miles, none of that fancy GPS stuff here
+            'location': '2',
             'r_cycle': '10',
             'g_cycle': '40',
-            'offset': '0', # offset from an external decided time for the cycles to start
+            'offset': '0',
         }
-        
+
         for key, default in defaults.items():
             setattr(self, key, kwargs.get(key, default))
 
-    def tell_location(self):
-        return 'This signal is located at mile marker {}.'.format(self.location)
-        
-    def tell_r_cycle(self):
-        return 'The red light cycle for this signal {} seconds.'.format(self.r_cycle)
-        
-    def tell_g_cycle(self):
-        return 'The green light cycle for this signal is {} seconds.'.format(self.g_cycle)
-    
-    def tell_offset(self):
-        return 'The time offset for this signal is {} seconds.'.format(self.offset)
-        
-    @staticmethod    
-    def generate_random_signal_data(min_location = 1, max_location = 10):
+    def state_location(self):
+        """
+        Returns a formatted string stating the location of the signal on a road
+        """
+        line = 'This signal is located at mile marker {}.'
+        return line.format(self.location)
+
+    def state_r_cycle(self):
+        """
+        Returns a formatted string stating the red light cycle in seconds
+        """
+        line = 'The red light cycle for this signal {} seconds.'
+        return line.format(self.r_cycle)
+
+    def state_g_cycle(self):
+        """
+        Returns a formatted string stating the green light cycle in seconds
+        """
+        line = 'The green light cycle for this signal is {} seconds.'
+        return line.format(self.g_cycle)
+
+    def state_offset(self):
+        """
+        Returns a formatted string stating the offset in seconds
+        """
+        line = 'The time offset for this signal is {} seconds.'
+        return line.format(self.offset)
+
+    @staticmethod
+    def generate_random_signal_data(min_location=1, max_location=10):
+        """
+        Generate random signal data to pass to a new signal object
+
+        Args:
+            min_location: Minimum mile marker for signal placement
+            max_location: Maximum mile amrker for signal placement
+
+        Returns:
+            A dict of data to pass to a new signal object
+        """
         data = {
-            'location': random.randint(min_location, max_location), 
-            'r_cycle': random.randint(10, 30), 
+            'location': random.randint(min_location, max_location),
+            'r_cycle': random.randint(10, 30),
             'g_cycle': random.randint(25, 95),
             'offset': random.randint(0, 30),
         }
-        
-        return data
 
+        return data
 
 if __name__ == "__main__":
     data = Signal.generate_random_signal_data(1, 15)
     x = Signal(**data)
-    print(x.tell_location())
-    print(x.tell_r_cycle())
-    print(x.tell_g_cycle())
-    print(x.tell_offset())
+    print(x.state_location())
+    print(x.state_r_cycle())
+    print(x.state_g_cycle())
+    print(x.state_offset())
     y = Signal()
-    print(y.tell_location())
-    print(y.tell_r_cycle())
-    print(y.tell_g_cycle())
-    print(y.tell_offset())
-    
+    print(y.state_location())
+    print(y.state_r_cycle())
+    print(y.state_g_cycle())
+    print(y.state_offset())
